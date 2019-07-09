945199e88a51b4d146c945601cf15361c068eed1
Adriano Souza Brandão <asbmails2@gmail.com>
2019-07-09 13:04:29 -0300

    adding POX

--- ENTRYPOINT.sh
+++ ENTRYPOINT.sh
@@ -3,6 +3,13 @@
 service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
 
+#install POX
+cd ~
+git clone http://github.com/noxrepo/pox
+cd pox  
+git checkout dart
+cd ~
+
 bash
 
 service openvswitch-switch stop
